e5bb8715bb3ee1b69a4f0e4e14795ef61fb2cbb8
vonundzuBergen <c.bergen@gmx.net>
2021-05-01 01:26:32 +0200

    Split path parameter of data ingestion functions

--- yellowcab/io/input.py
+++ yellowcab/io/input.py
@@ -14,37 +14,42 @@ def read_file(path=os.path.join(get_data_path(), "input/trip_data", "<My_data>.p
         print("Data file not found. Path was " + path)
 
 
-def read_parquet(path=os.path.join(get_data_path(), "input/trip_data"), file):
+def read_parquet(file, base_path=get_data_path(), relative_path="input/trip_data"):
     """
     This function reads a parquet file & returns it as a pd.DataFrame.
     ----------------------------------------------
     :param
-        path(String): Path to directory. Defaults to wd/data/input/trip_data.
         file(String): Name of file
+        base_path(String): Path to data directory. Defaults to wd/data.
+        relative_path(String): Path to directory with file in base_path. Defaults to input/trip_data.
     :returns
-        pd.DataFrame: DataFrame containing data from parquet.
+        pd.DataFrame: DataFrame containing data from a single parquet.
     """
+    path = os.path.join(base_path, relative_path, file)
     try:
-        table = pq.read_table(os.path.join(path, file))
+        table = pq.read_table(path)
         df = table.to_pandas()
         return df
     except FileNotFoundError:
         print("Data file not found. Path was " + path)
 
 
-def read_parquet_dataset(path=os.path.join(get_data_path(), "input/trip_data")):
+def read_parquet_dataset(base_path=get_data_path(), relative_path="input/trip_data"):
     """
     This function reads a directory of parquet files & returns them as a single pd.DataFrame.
     ----------------------------------------------
     :param
-        path(String): Path to directory. Defaults to wd/data/input/trip_data.
+        base_path(String): Path to data directory. Defaults to wd/data.
+        relative_path(String): Path to directory with parquet dataset in base_path. Defaults to input/trip_data.
     :returns
-        pd.DataFrame: DataFrame containing data from all parquet files in path.
+        pd.DataFrame: DataFrame containing random sample data from all parquet files in path.
     """
+    path = os.path.join(base_path, relative_path)
     try:
         dataset = pq.ParquetDataset(path)
         table = dataset.read()
         df = table.to_pandas()
+        df = df.reset_index()
         return df
     except FileNotFoundError:
         print("Data file not found. Path was " + path)
