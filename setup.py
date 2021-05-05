87d59cf5c1535024fe00df3e2d1ecd45e4c577ad
Nina <n.erlacher@web.de>
2021-05-05 20:17:19 +0200

    Hot fix and setup.py extended

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import find_packages, setup
 
+requirements = ["pandas", "numpy", "click", "scikit-learn"]
+
 setup(
     name="yellowcab",
     version="0.0.1dev1",
     description="Semester Project - Programming Data Science",
-    author="Student",
-    author_email="student@uni-koeln.de",
+    url="https://github.com/SepehrRad/PDS_TestProject_2021",
+    author="Christian Bergen, Nina Annika Erlacher, Nico Liedmeyer, Sepehr Salemirad, Simon Maximilian Wolf",
+    author_email="ssalemir@smail.uni-koeln.de",
     packages=find_packages(include=["yellowcab", "yellowcab.*"]),
-    install_requires=["pandas", "numpy", "click", "scikit-learn"],
+    install_requires=requirements,
     entry_points={"console_scripts": ["yellowcab=yellowcab.cli:main"]},
+    keywords="PDS",
+    classifiers=[
+        "Development Status :: 1 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: GPL-3.0 License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Education",
+    ]
 )
