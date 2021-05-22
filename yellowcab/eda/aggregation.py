ed7ea630e190cbd6cbf661209cd0c64ab8940af0
vonundzuBergen <c.bergen@gmx.net>
2021-05-22 13:32:14 +0200

    minor bugfixes
    agg_functions: removed mutable object as default parameter and made sure, that returned object is always a DF

--- yellowcab/eda/aggregation.py
+++ yellowcab/eda/aggregation.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 
 def agg_stats(
-    group_series,
-    data_series,
-    agg_functions=["min", "max", "mean", "median", "std", "var", "sem"],
+        group_series,
+        data_series,
+        agg_functions=None,
 ):
     """
     This function calculates (user specified) aggregated statistics of two input series & returns them as DataFrame.
@@ -12,15 +12,19 @@ def agg_stats(
     :param
         group_series(pd.Series): Data you want to group by.
         data_series(pd.Series): Data you want aggregated.
-        agg_functions(list[String]): Aggregates data based on these functions. Default is
+        agg_functions(list[String] or String): Aggregates data based on given functions. Default is
         ["min", "max", "mean", "median","std", "var", "sem"]. For more information use pandas groupby documentation.
     :returns
         pd.DataFrame: DataFrame containing aggregated statistics.
     """
+    if agg_functions is None:
+        agg_functions = ["min", "max", "mean", "median", "std", "var", "sem"]
     time_series_name = group_series.name
     data_series_name = data_series.name
     df = pd.concat([group_series, data_series], axis=1)
     df = df.groupby(time_series_name)[data_series_name].agg(agg_functions)
+    if isinstance(df, pd.Series):
+        df = df.to_frame(name=agg_functions)
     df = df.add_suffix("_" + data_series_name)
     return df
 
