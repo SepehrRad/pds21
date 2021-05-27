9a096a9a0b7e7d967ae1396ffea0a3aeef5bfae6
Nina <n.erlacher@web.de>
2021-05-27 23:05:56 +0200

    baseline model for 2. regression task, expanded documentation

--- yellowcab/prediction/prediction.py
+++ yellowcab/prediction/prediction.py
@@ -5,6 +5,7 @@ from yellowcab.cleaning import clean_dataset
 from yellowcab.io.output import write_parquet
 from yellowcab.io.utils import get_data_path
 from yellowcab.preprocessing import transform_columns
+from yellowcab.io.output import save_model
 from tqdm import tqdm
 from os import listdir
 from os.path import isfile, join
@@ -20,44 +21,11 @@ from sklearn import metrics
 import pandas as pd
 
 #%%
-
-def clean_all_datasets(base_path=get_data_path(), relative_path="input/trip_data",verbose=False):
-    """
-
-    :param base_path:
-    :param relative_path:
-    :param verbose:
-    :return:
-    """
-    data_path = join(base_path, relative_path)
-    data_sets = [dataset for dataset in listdir(data_path) if isfile(join(data_path, dataset))]
-    if not all('.parquet' in name for name in data_sets):
-        raise ValueError("The given directory includes non parquet files")
-    for parquet_file in tqdm(data_sets):
-        #assumes 01.parquet,02.parquet,...
-        month=int(parquet_file.split('.parquet')[0])
-        print(f'Started cleaning the {calendar.month_name[month]} data set')
-        cleaned_df = clean_dataset(read_parquet(parquet_file),month=month,verbose=verbose)
-        write_parquet(cleaned_df,filename=f'{calendar.month_name[month]}_cleaned.parquet')
-        print(f'Finished cleaning the {calendar.month_name[month]} data set')
-
-
-#%%
-
-cab_df = read_parquet_dataset(relative_path="input/cleaned").sample(100,random_state=7)
-#cab_df = cab_df.loc[~(cab_df['payment_type']=='Unknown')]
-#Weekend to int
-
-#%%
-cab_df.info()
-
-
-#%%
 def _get_random_state():
     """
     This function defines our random state, so that it's the same in
     every application
-
+    ----------------------------------------------
     :return:
         int: the integer used for random state paramenter
     """
@@ -67,6 +35,7 @@ def _get_random_state():
 def _get_column_description_for_prediction():
     """
     This function defines the categories our features belong to.
+    ----------------------------------------------
     :return:
         dictionary: Dictionary containing our feature categories
                     with the associated attributes.
@@ -85,6 +54,7 @@ def _make_data_preparation(df, prediction_type, target):
     TODO: As soon as Simon has corrected weekend in cleaning, line can be removed.
     This function reduces the dataframe to one containing only relevant features
     for prediction purposes.
+    ----------------------------------------------
     :param df (pandas.DataFrame): the given pandas data frame with all initial features
            prediction_type (String): Denotes whether used for regression or classification.
            :param target: Dependent variable for prediction purposes.
@@ -93,30 +63,41 @@ def _make_data_preparation(df, prediction_type, target):
 
     """
     if prediction_type == "regression":
-        column_description = _get_column_description_for_prediction()
-        df = transform_columns(df,column_description) #for what transform columns?
-        df.drop(column_description.get('spatial_features'),inplace=True, axis=1)
-        df.drop(column_description.get('temporal_features'),inplace=True, axis=1)
-        df.pop('weekend') # remove after Simons correction
+        if target == "trip_distance":
+            column_description = _get_column_description_for_prediction()
+            new_df = transform_columns(df,column_description) #for what transform columns?
+            new_df.drop(column_description.get('spatial_features'), inplace=True, axis=1)
+            new_df.drop(column_description.get('temporal_features'),inplace=True, axis=1)
+            new_df.pop('weekend') # remove after Simons correction
+        if target == "fare_amount":
+            column_description = _get_column_description_for_prediction()
+            new_df = transform_columns(df, column_description)
+            column_description["categorical_features"] = new_df.loc[:, new_df.columns.str.contains(
+                '^Rate')].columns.tolist(), new_df.loc[:, new_df.columns.str.contains('^pay')].columns.tolist()
+            column_description["categorical_features"] = sum(column_description["categorical_features"], [])
+            new_df.drop(column_description.get('categorical_features'), inplace=True, axis=1)
+            new_df.drop(column_description.get('temporal_features'), inplace=True, axis=1)
+
     else:
         column_description = _get_column_description_for_prediction()
         column_description['categorical_features'] = ['RatecodeID'] #payment type should not be transofrmed here
-        df = transform_columns(df,column_description) #for what transform columns?
-        df.drop(column_description.get('spatial_features'),inplace=True, axis=1)
-        df.drop(column_description.get('temporal_features'),inplace=True, axis=1)
-        df.pop('weekend') # remove after Simons correction
+        new_df = transform_columns(df,column_description) #for what transform columns?
+        new_df.drop(column_description.get('spatial_features'),inplace=True, axis=1)
+        new_df.drop(column_description.get('temporal_features'),inplace=True, axis=1)
+        new_df.pop('weekend') # remove after Simons correction
 
     #if prediction_type == "regression":
     #    df = pd.get_dummies(df)
-    return df
+    return new_df
 
 def _make_pipeline(model,scaler_type, numerical_features, model_name, use_sampler=False, sampler=None):
     """
     This function assembles several steps that can be cross-validated together
     while setting different parameters.
+    ----------------------------------------------
     :param prediction_type (String):
         Denotes whether used for regression or classification.
-    :param use_sampler:
+    :param use_sampler: For testing whether the use of over-/ under-sampling improves our metrics.
     :param model: Used model for prediction.
     :return: Pipeline: Sequentially applies the list of transforms
              and a final estimator.
@@ -146,6 +127,7 @@ def _make_train_test_split(df,target):
     """
     This function splits the input data set into a train an a test set, each
     for the regressors X and the dependent variable y.
+    ----------------------------------------------
     :param df (pandas.DataFrame): the given pandas data frame containing data which
                                   need to be split into train and test data sets.
     :param target: Dependent variable for prediction purposes.
@@ -165,6 +147,7 @@ def _print_prediction_scores(prediction_type, y_test, X_test, pipeline):
     """
     This function prints the prediction scores for either a regression or
     a classification.
+    ----------------------------------------------
     :param prediction_type (String): Denotes whether used for regression or classification.
     :param y_test (pandas.Series): Target values for testing a model
     :param X_test (pandas.DataFrame): Regressors used for testing a model
@@ -185,6 +168,7 @@ def make_predictions(df, prediction_type, target, model, model_name, scaler_type
         """
         This function predicts and prints the prediction scores of a prediction
         task dynamically defined by the input parameters.
+        ----------------------------------------------
         :param df (pandas.DataFrame): the given pandas data frame containing data
                                       used for prediction.
         :param prediction_type (String): Denotes whether used for regression or classification.
@@ -192,12 +176,16 @@ def make_predictions(df, prediction_type, target, model, model_name, scaler_type
         :return:
         """
         df = _make_data_preparation(df, prediction_type, target = target)
-        X_train, X_test, y_train, y_test = _make_train_test_split(df=df,target=target)
         numerical_features = df.select_dtypes(include=['float64']).columns
+        X_train, X_test, y_train, y_test = _make_train_test_split(df=df, target=target)
         pipeline = _make_pipeline(model = model, model_name = model_name, scaler_type =scaler_type,
                                   numerical_features= numerical_features,
                                   use_sampler=use_sampler, sampler=sampler) #parameter overloading
+        if use_sampler:
+            name = sampler
+            X_train, y_train = name.fit_sample(X_train, y_train)
         pipeline = pipeline.fit(X_train, y_train)
+        save_model(model)
         _print_prediction_scores(prediction_type=prediction_type, y_test=y_test, X_test=X_test,
                                  pipeline=pipeline)
 
@@ -207,28 +195,41 @@ def make_baseline_predictions(df):
     This function performs baseline predictions for both classification and
     regression. Those can be used to evaluate fall other machine learning algorithms,
     as it provides the required point of comparison.
+    ----------------------------------------------
     :param df (pandas.DataFrame): the given pandas data frame containing data
                                   used for prediction.
     :return:
     """
     classification_model = LogisticRegression(multi_class="multinomial", class_weight="balanced",
                                               n_jobs=-1, random_state=_get_random_state(), max_iter=5000)
-    regression_model_1 = LinearRegression()
-    #regression_model_2 = LinearRegression()
+    regression_model = LinearRegression()
+
+    # classification for "payment_type"
     make_predictions(
         df=df, prediction_type="classification", target ="payment_type", model = classification_model,
         scaler_type= "standard_scaler", model_name="Classification"
     )
+
+    # classification for "payment_type" using under-sampling (near-miss)
+
+
+    # regression for "trip_distance"
     make_predictions(
-        df=df, prediction_type="regression", target ="trip_distance", model = regression_model_1,
+        df=df, prediction_type="regression", target ="trip_distance", model = regression_model,
         scaler_type= "standard_scaler", model_name="Regression 1"
     )
 
-make_baseline_predictions(cab_df)
+    # regression for "fare_amount"
+    make_predictions(
+        df=df, prediction_type="regression", target="fare_amount", model=regression_model,
+        scaler_type="standard_scaler", model_name="Regression 2"
+    )
 
 
-# NEAR MISS anstatt classbased balane
-#save to pickle für alle modelle
+# NEAR MISS anstatt classbased balane done but not wirking
+# save to pickle für alle modelle done
+# prediction documented done
+# phillips functions documented
 
 #%%
 
