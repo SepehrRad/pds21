687524197d00182b520999edf30799f7db0d9d16
Nina <n.erlacher@web.de>
2021-05-28 11:11:16 +0200

    sampler for baseline classification implemented

--- yellowcab/prediction/prediction.py
+++ yellowcab/prediction/prediction.py
@@ -1,15 +1,8 @@
 #%%
+from imblearn.under_sampling import NearMiss
 from sklearn.model_selection import train_test_split
-from yellowcab.io.input import read_parquet_dataset,read_parquet, read_geo_dataset
-from yellowcab.cleaning import clean_dataset
-from yellowcab.io.output import write_parquet
-from yellowcab.io.utils import get_data_path
 from yellowcab.preprocessing import transform_columns
 from yellowcab.io.output import save_model
-from tqdm import tqdm
-from os import listdir
-from os.path import isfile, join
-import calendar
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.compose import ColumnTransformer
@@ -18,7 +11,6 @@ from imblearn.metrics import classification_report_imbalanced
 from sklearn.linear_model import LogisticRegression, LinearRegression
 import numpy as np
 from sklearn import metrics
-import pandas as pd
 
 #%%
 def _get_random_state():
@@ -40,7 +32,6 @@ def _get_column_description_for_prediction():
         dictionary: Dictionary containing our feature categories
                     with the associated attributes.
     """
-    #why not all features listed?
     column_description = {'cyclical_features':['pickup_month','pickup_day','pickup_hour',
                                                'dropoff_hour','dropoff_day','dropoff_month'],
                       'categorical_features':['RatecodeID', 'payment_type'],
@@ -65,7 +56,7 @@ def _make_data_preparation(df, prediction_type, target):
     if prediction_type == "regression":
         if target == "trip_distance":
             column_description = _get_column_description_for_prediction()
-            new_df = transform_columns(df,column_description) #for what transform columns?
+            new_df = transform_columns(df,column_description)
             new_df.drop(column_description.get('spatial_features'), inplace=True, axis=1)
             new_df.drop(column_description.get('temporal_features'),inplace=True, axis=1)
             new_df.pop('weekend') # remove after Simons correction
@@ -77,20 +68,16 @@ def _make_data_preparation(df, prediction_type, target):
             column_description["categorical_features"] = sum(column_description["categorical_features"], [])
             new_df.drop(column_description.get('categorical_features'), inplace=True, axis=1)
             new_df.drop(column_description.get('temporal_features'), inplace=True, axis=1)
-
     else:
         column_description = _get_column_description_for_prediction()
         column_description['categorical_features'] = ['RatecodeID'] #payment type should not be transofrmed here
-        new_df = transform_columns(df,column_description) #for what transform columns?
+        new_df = transform_columns(df,column_description)
         new_df.drop(column_description.get('spatial_features'),inplace=True, axis=1)
         new_df.drop(column_description.get('temporal_features'),inplace=True, axis=1)
-        new_df.pop('weekend') # remove after Simons correction
-
-    #if prediction_type == "regression":
-    #    df = pd.get_dummies(df)
+        new_df.pop('weekend') #remove after Simons correction
     return new_df
 
-def _make_pipeline(model,scaler_type, numerical_features, model_name, use_sampler=False, sampler=None):
+def _make_pipeline(model,scaler_type, numerical_features, model_name, sampler_name = None, use_sampler=False, sampler=None):
     """
     This function assembles several steps that can be cross-validated together
     while setting different parameters.
@@ -117,13 +104,11 @@ def _make_pipeline(model,scaler_type, numerical_features, model_name, use_sample
         transformers=[('num', preprocessor_pipeline, numerical_features)])
     preprocessor = ("preprocessor", preprocessor_pipeline)
     steps.append(preprocessor)
-    if use_sampler:
-        steps.append(sampler)
     prediction_model=(model_name,model)
     steps.append(prediction_model)
     return Pipeline(steps=steps)
 
-def _make_train_test_split(df,target):
+def _make_train_test_split(df,target, use_sampler, sampler):
     """
     This function splits the input data set into a train an a test set, each
     for the regressors X and the dependent variable y.
@@ -141,6 +126,9 @@ def _make_train_test_split(df,target):
     X = df
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=_get_random_state())
+    if use_sampler:
+        test = sampler
+        X_train, y_train = test.fit_resample(X_train, y_train)
     return X_train, X_test, y_train, y_test
 
 def _print_prediction_scores(prediction_type, y_test, X_test, pipeline):
@@ -162,8 +150,7 @@ def _print_prediction_scores(prediction_type, y_test, X_test, pipeline):
         print("R2: ", metrics.r2_score(y_test, pipeline.predict(X_test)))
 
 
-
-def make_predictions(df, prediction_type, target, model, model_name, scaler_type, use_sampler=False,
+def make_predictions(df, prediction_type, target, model, model_name, scaler_type, sampler_name=None, use_sampler=False,
                      sampler=None):
         """
         This function predicts and prints the prediction scores of a prediction
@@ -177,18 +164,14 @@ def make_predictions(df, prediction_type, target, model, model_name, scaler_type
         """
         df = _make_data_preparation(df, prediction_type, target = target)
         numerical_features = df.select_dtypes(include=['float64']).columns
-        X_train, X_test, y_train, y_test = _make_train_test_split(df=df, target=target)
+        X_train, X_test, y_train, y_test = _make_train_test_split(df=df, target=target, sampler=sampler, use_sampler=use_sampler)
         pipeline = _make_pipeline(model = model, model_name = model_name, scaler_type =scaler_type,
                                   numerical_features= numerical_features,
-                                  use_sampler=use_sampler, sampler=sampler) #parameter overloading
-        if use_sampler:
-            name = sampler
-            X_train, y_train = name.fit_sample(X_train, y_train)
+                                  use_sampler=use_sampler, sampler=sampler, sampler_name=sampler_name) #parameter overloading
         pipeline = pipeline.fit(X_train, y_train)
         save_model(model)
         _print_prediction_scores(prediction_type=prediction_type, y_test=y_test, X_test=X_test,
                                  pipeline=pipeline)
-
 
 def make_baseline_predictions(df):
     """
@@ -204,6 +187,8 @@ def make_baseline_predictions(df):
                                               n_jobs=-1, random_state=_get_random_state(), max_iter=5000)
     regression_model = LinearRegression()
 
+    nr = NearMiss()
+
     # classification for "payment_type"
     make_predictions(
         df=df, prediction_type="classification", target ="payment_type", model = classification_model,
@@ -211,7 +196,10 @@ def make_baseline_predictions(df):
     )
 
     # classification for "payment_type" using under-sampling (near-miss)
-
+    make_predictions(
+        df=df, prediction_type="classification", target="payment_type", model=classification_model,
+        scaler_type="standard_scaler", model_name="Classification", use_sampler=True, sampler= nr
+    )
 
     # regression for "trip_distance"
     make_predictions(
@@ -226,8 +214,8 @@ def make_baseline_predictions(df):
     )
 
 
-# NEAR MISS anstatt classbased balane done but not wirking
-# save to pickle für alle modelle done
+# NEAR MISS instead of  class-based balance done
+# save to pickle for all models done
 # prediction documented done
 # phillips functions documented
 
